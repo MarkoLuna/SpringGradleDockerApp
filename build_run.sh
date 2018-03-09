13c8644173657ab3b11b55d546d9df35958aa288
jlunav <jlunav@EKT1201843.ad.elektra.com.mx>
2018-03-09 14:47:01 -0600

    Update build script

--- build_run.sh
+++ build_run.sh
@@ -3,6 +3,6 @@
 APP_CONTAINER_NAME=myapp
 PORT_NUMBER=8080
 
-./gradlew build &&
-docker build -t $APP_CONTAINER_NAME . &&
+./gradlew build && \
+docker build -t $APP_CONTAINER_NAME . && \
 docker run -it --rm -p $PORT_NUMBER:$PORT_NUMBER $APP_CONTAINER_NAME
